877fa677cbb729eef3ce7002f8499100ad08c1a6
Zeke_ZB <zekiel.villasurda@gmail.com>
2026-02-05 10:48:23 +0800

    Implement Templating

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- mysite/task/views.py
+++ mysite/task/views.py
@@ -6,3 +6,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the task index.")
+
+def task_list(request):
+    ctx = {
+        "tasks": [
+            "task 1",
+            "task 2",
+            "task 3",
+            "task 4"
+        ]
+    }
+    return render(request, 'tasks/task_list.html', ctx)
