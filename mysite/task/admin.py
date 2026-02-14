586139fe375b569861d38146376965a8116d25cd
Zeke_ZB <zekiel.villasurda@gmail.com>
2026-02-15 04:32:01 +0800

    Use Models in Task App

--- mysite/task/admin.py
+++ mysite/task/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TaskGroup, Task
 
 # Register your models here.
+
+admin.site.register(TaskGroup)
+admin.site.register(Task)
--- mysite/task/views.py
+++ mysite/task/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from .models import TaskGroup, Task
 
 # Create your views here.
 
@@ -8,7 +9,7 @@ from django.http import HttpResponse
 tasks: list[str] = ["task 1", "task 2", "task 3", "task 4"]
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the task index.")
+    return redirect('task-list')
 
 def task_list(request):
     ctx = {"tasks": tasks}
@@ -22,11 +23,17 @@ class TaskListView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tasks'] = tasks
+        context['tasks'] = Task.objects.all()
         return context
 
     def post(self, request, *args, **kwargs):
         # print(request.POST.get('task_name'))
-        tasks.append(request.POST.get('task_name'))
+        task_name = request.POST.get('task_name')
+        if task_name:
+            Task.objects.create(
+                name=task_name,
+                due_date="2026-12-31T23:59:59",
+                taskgroup=TaskGroup.objects.first()   # Assuming there is at least one TaskGroup
+            )
         context = self.get_context_data(**kwargs)
         return self.get(request, *args, **kwargs)
