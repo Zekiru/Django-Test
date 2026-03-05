0c877658dd38790726057adc7e05c9eead117af0
Zeke_ZB <zekiel.villasurda@gmail.com>
2026-03-05 14:28:01 +0800

    Non-standard Form Implementation

--- mysite/task/views.py
+++ mysite/task/views.py
@@ -18,14 +18,17 @@ class TaskListView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = Task.objects.all()
+        context['taskgroups'] = TaskGroup.objects.all()
         return context
 
     def post(self, request, *args, **kwargs):
         # print(request.POST.get('task_name'))
         task_name = request.POST.get('task_name')
+        due_date = request.POST.get('due_date')
+        taskgroup = request.POST.get('taskgroup')
         Task.objects.create(
             name=task_name,
-            due_date="2026-12-31T23:59:59",
-            taskgroup=TaskGroup.objects.first()
+            due_date=due_date,
+            taskgroup=TaskGroup.objects.get(id=taskgroup)
         )
         return redirect('task-list')
